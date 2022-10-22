1be0e33883da2f51058709529520a265e62b77ee
YeahQing <joashzhai@outlook.com>
2022-10-23 00:06:22 +0800

    feat: add leetcode-160.

---
+++ src/com/yeahqing/easy/_160/Solution.java
@@ -0,0 +1,37 @@
+package com.yeahqing.easy._160;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author YeahQing
+ * @date 2022/10/22
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<>();
+        ListNode temp = headA;
+        while (temp != null) {
+            visited.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
