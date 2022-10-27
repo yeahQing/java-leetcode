dcaa3b24ea5d61f83b1b21ccd4779e14aa6dcf44
YeahQing <joashzhai@outlook.com>
2022-10-27 22:20:22 +0800

    feat: add LeetCode1239.

--- src/com/yeahqing/medium/_1239/LeetCode1239.java
+++ src/com/yeahqing/medium/_1239/LeetCode1239.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class LeetCode1239 {
     public static void main(String[] args) {
-        Solution solution = new Solution1();
+        Solution solution = new Solution2();
         List<String> testCase = new ArrayList<>(Arrays.asList("un", "iq", "ue"));
         System.out.println(solution.maxLength(testCase));
     }
@@ -59,6 +59,7 @@ class Solution {
 class Solution1 extends Solution {
     @Override
     public int maxLength(List<String> arr) {
+        // 使用哈希集合去重
         List<HashSet<Character>> all = new ArrayList<>();
         all.add(new HashSet<>());
         int ans = 0;
@@ -74,6 +75,40 @@ class Solution1 extends Solution {
                 if (tmp.size() == s.size() + str.length()) {
                     all.add(tmp);
                     ans = Math.max(ans, tmp.size());
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+class Solution2 extends Solution {
+    @Override
+    public int maxLength(List<String> arr) {
+        int ans = 0;
+        List<Integer> masks = new ArrayList<>();
+        masks.add(0); // 空串
+
+        for (String s : arr) {
+            // 1. 判断字符串中有没有重复的字符
+            int mask = 0;
+            for (Character c : s.toCharArray()) {
+                int ch = c - 'a';
+                if ((mask & 1 << ch) != 0) {
+                    mask = 0;
+                    break;
+                }
+                // 没有重复字符，当前字符串就是可行解的一种
+                mask |= 1 << ch;
+            }
+            // 有重复字符，直接跳过
+            if (mask == 0) continue;
+            // 2. 和前i项可行解做拼接，判断是否满足条件
+            for (int i = 0; i < masks.size(); i++) {
+                if ((mask & masks.get(i)) == 0) {
+                    // 和空串拼接，就是将当前串添加到可行解中
+                    masks.add(mask | masks.get(i));
+                    ans = Math.max(ans, Integer.bitCount(mask | masks.get(i)));
                 }
             }
         }
